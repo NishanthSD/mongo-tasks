031bac71a2a68221be62ef54117c828003cd6709
NishanthSD <nishanthsdedu@gmail.com>
2025-01-17 23:22:14 +0530

    GET, POST done, index.html still needs some work

---
+++ index.js
@@ -0,0 +1,47 @@
+import { MongoClient } from "mongodb";
+import { config } from "dotenv";
+import express from "express";
+import path from "path";
+
+config();
+
+const uri = process.env.DB_URI;
+const client = new MongoClient(uri);
+
+client.connect();
+
+const db = client.db("nish-tasks");
+const coll = db.collection("tasks");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/create", (req, res) => {
+  console.log("POST");
+  try {
+    const document = req.body;
+    coll.insertOne(document);
+    res.write("SUCCESS...");
+  } catch (error) {
+    res.write("ERROR : ");
+    res.write(JSON.stringify(error));
+  } finally {
+    res.end();
+  }
+});
+
+app.get("/all_records", (req, res) => {
+  const docs = coll.find().toArray();
+  docs
+    .then((doc) => {
+      res.write(JSON.stringify(doc));
+      res.end();
+    })
+    .catch((err) => {
+      res.write(err);
+      res.end();
+    });
+});
+
+app.listen(3000);
